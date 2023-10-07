09b86099d424c452c7cda850714220b1e208b303
Niels Voss <code@nielsvoss.com>
2023-10-07 11:51:15 -0700

    Initial commit

---
+++ src/client/java/osbourn/holdyourbreath/HoldYourBreathClient.java
@@ -0,0 +1,10 @@
+package osbourn.holdyourbreath;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HoldYourBreathClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
