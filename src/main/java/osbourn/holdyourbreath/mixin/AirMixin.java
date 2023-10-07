ae0c09105a93147de6e276ad0959bc4500df718f
Niels Voss <code@nielsvoss.com>
2023-10-07 13:29:22 -0700

    Rename ExampleMixin to AirMixin

---
+++ src/main/java/osbourn/holdyourbreath/mixin/AirMixin.java
@@ -0,0 +1,9 @@
+package osbourn.holdyourbreath.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LivingEntity.class)
+public class AirMixin {
+
+}
