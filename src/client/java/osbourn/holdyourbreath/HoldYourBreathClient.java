6bc9c10c548c434b7bbb60e4d6c49451221c09d5
Niels Voss <code@nielsvoss.com>
2023-10-07 12:40:42 -0700

    Track when key is pressed and released

--- src/client/java/osbourn/holdyourbreath/HoldYourBreathClient.java
+++ src/client/java/osbourn/holdyourbreath/HoldYourBreathClient.java
@@ -1,13 +1,17 @@
 package osbourn.holdyourbreath;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class HoldYourBreathClient implements ClientModInitializer {
 	private static KeyBinding breatheKeyBinding;
+	private static boolean isHoldingBreath = false;
 
 	@Override
 	public void onInitializeClient() {
@@ -18,5 +22,23 @@ public class HoldYourBreathClient implements ClientModInitializer {
 				GLFW.GLFW_KEY_R,
 				"category.holdyourbreath.keybindings"
 		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (breatheKeyBinding.isPressed() && !isHoldingBreath) {
+				startHoldingBreath(client);
+			}
+			if (!breatheKeyBinding.isPressed() && isHoldingBreath) {
+				stopHoldingBreath(client);
+			}
+			isHoldingBreath = breatheKeyBinding.isPressed();
+		});
+	}
+
+	private void startHoldingBreath(MinecraftClient client) {
+		client.player.sendMessage(Text.literal("Started holding breath"));
+	}
+
+	private void stopHoldingBreath(MinecraftClient client) {
+		client.player.sendMessage(Text.literal("Stopped holding breath"));
 	}
 }
